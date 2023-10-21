e837e07c822dbbf04d646165f614c473839074c6
AnirudhDasari23 <148602591+AnirudhDasari23@users.noreply.github.com>
2023-10-20 22:26:30 -0500

    AND_gate.v

---
+++ AND_gate.v
@@ -0,0 +1,27 @@
+module andgat (a, x, y, z):
+input [1:0] a;
+output x, y, z;
+assign x = a[0] & a[1];
+and (y, a[0],a[1]);
+reg z;
+always@ (a)
+begin
+if(a[0]=0 & a[1]==0)
+begin
+z = 0
+end
+if(a[0]==0 & a[1]==1)
+begin
+z = 0 ;
+end
+if(a[0]=1 a[1]==0)
+begin
+z = 0
+end
+if( a[0] = 1 a[1]==1)
+begin
+z = 1
+end
+end
+endmodule
+
